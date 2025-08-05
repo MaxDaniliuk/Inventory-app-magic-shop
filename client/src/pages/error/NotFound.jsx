3c759f85537fb20ccbe3835f06aa4f0dd615800d
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2025-08-05 23:29:35 +0300

    Make a message in the NotFound page more visible

--- client/src/pages/error/NotFound.jsx
+++ client/src/pages/error/NotFound.jsx
@@ -2,10 +2,25 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div className="error not-found">
+    <div
+      className="error not-found"
+      style={{
+        textAlign: 'center',
+        fontSize: '1.5rem',
+      }}
+    >
       <p>There is nothing to look at...</p>
       <p>
-        Retrun to the <Link to="/">Homepage</Link>
+        Retrun to the{' '}
+        <Link to="/">
+          <span
+            style={{
+              fontWeight: 'bold',
+            }}
+          >
+            → Homepage ←
+          </span>
+        </Link>
       </p>
     </div>
   );
